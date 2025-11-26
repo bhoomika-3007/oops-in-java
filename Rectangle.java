e3209bba1fd1a407982519f6d7aa4c6067e09b05
Bhoomika Sharma <bharti261220@gmail.com>
2025-11-26 21:42:51 +0530

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,25 @@
+//Interface example
+
+interface Drawable {
+    void draw();
+}
+
+class Circle implements Drawable {
+    public void draw() {
+        System.out.println("Drawing Circle");
+    }
+}
+
+class Rectangle implements Drawable {
+    public void draw() {
+        System.out.println("Drawing Rectangle");
+    }
+
+    public static void main(String[] args) {
+        Drawable d1 = new Circle();
+        Drawable d2 = new Rectangle();
+
+        d1.draw();
+        d2.draw();
+    }
+}
